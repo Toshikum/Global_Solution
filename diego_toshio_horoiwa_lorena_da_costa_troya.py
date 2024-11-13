a368b1ef4b310606b07e42d51248824001408178
Diego Toshio <83375809+Toshikum@users.noreply.github.com>
2024-11-13 09:07:36 -0300

    Segundo Commit
    Funcionalidade Metas Cumpridas adicionada!

--- diego_toshio_horoiwa_lorena_da_costa_troya.py
+++ diego_toshio_horoiwa_lorena_da_costa_troya.py
@@ -3,6 +3,8 @@ maior = 0
 menor = 0
 media = 0
 soma_de_consumo = 0
+cumpriram = 0
+nao_cumpriram = 0
 meta = 150
 
 for d in range (1, dias + 1):
@@ -20,6 +22,21 @@ for d in range (1, dias + 1):
         if consumo < menor:
             menor = consumo
 
+    if consumo >= meta:
+        cumpriram += 1
+    else:
+        nao_cumpriram += 1
+
+
 media = soma_de_consumo/dias
+
+if cumpriram == dias:
+    print(f'Parabéns! Todos os dias cumpriram a meta de consumo sustentável.')
+
+elif nao_cumpriram == dias:
+    print(f'Infelizmente, nenhum dia cumpriu a meta de consumo sustentável.')
+
+else:
+    print(f'{cumpriram} dias cumpriram a meta e {nao_cumpriram} dias não cumpriram a meta.')
 
 print(f'A média de consumo foi de {media:.2f} kWh. O maior consumo foi de {maior} kWh e o menor consumo foi de {menor} kWh')
